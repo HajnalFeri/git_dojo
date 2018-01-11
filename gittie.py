27bc1996fd8166d05c4ce6390db10f51a6b8ccce
HajnalFeri <kalacsf1@gmail.com>
2018-01-11 09:46:15 +0100

    full

--- gittie.py
+++ gittie.py
@@ -1,19 +1,24 @@
 def introduce():
     print("Hello I'm Kuki")
 
-introduce()
 
 def add(A, B):
     return A + B
 
-print(add(5, 4))
-    
+
 def joke():
     print("A jumper cable walks into a bar. The bartender says, I'll serve You, but don't start anything.")
+
 
 def shout():
     print("HELLO")
 
+
+def shout():
+    return("WÁÁÁÁÁÁÁÁÁÁÁ")
+
+
 introduce()
+print(add(5, 4))
+print(shout())
 joke()
-shout()
