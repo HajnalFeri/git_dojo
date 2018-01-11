0eb9db723d2bba3e41816e1c7af11c95632ce7da
Kontra László <kontravlaszlo@gmail.com>
2018-01-11 09:40:23 +0100

    shout function added

--- gittie.py
+++ gittie.py
@@ -11,5 +11,9 @@ print(add(5, 4))
 def joke():
     print("A jumper cable walks into a bar. The bartender says, I'll serve You, but don't start anything.")
 
+def shout():
+    print("HELLO")
+
 introduce()
 joke()
+shout()
