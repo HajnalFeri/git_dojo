df29fb342170b83860b8ac636713017f611a520c
Kontra László <kontravlaszlo@gmail.com>
2018-01-11 09:28:12 +0100

    best joke ever

--- gittie.py
+++ gittie.py
@@ -1,4 +1,8 @@
 def introduce():
     print("Hello I'm Gittie")
 
+def joke():
+    print("A jumper cable walks into a bar. The bartender says, I'll serve You, but don't start anything.")
+
 introduce()
+joke()
